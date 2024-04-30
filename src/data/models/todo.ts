e461000acbe7e9f94f767d889319596e11cf1e2e
diego-targino <diegotargino190@gmail.com>
2024-04-29 21:09:39 -0300

    refactor(models): alteração na entidade todo

--- src/data/models/todo.ts
+++ src/data/models/todo.ts
@@ -1,10 +1,10 @@
 import { ObjectId } from "mongodb";
 
 export class Todo {
-	constructor(
-		public description: string,
-		public completed: boolean,
-		public userId: ObjectId,
-		public _id?: ObjectId
-	) { }
+  constructor(
+    public description: string,
+    public completed: boolean,
+    public userId: string,
+    public _id?: ObjectId
+  ) {}
 }
